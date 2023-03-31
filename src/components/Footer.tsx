6004d96bb47b20e1c44f83e11945316250151561
Divine Orji <dpkreativ@gmail.com>
2023-03-31 03:45:14 +0100

    feat: a responsive footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+export function Footer() {
+  const [mode, setMode] = useState('auto');
+  return (
+    <div>
+      <footer id="footer" className="relative z-50 dark:bg-gray-50">
+        <div className="py-5 flex flex-col justify-center items-center">
+          <p className="text-xs lg:text-sm leading-none text-gray-900">
+            2021 Lyne. All Rights Reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
